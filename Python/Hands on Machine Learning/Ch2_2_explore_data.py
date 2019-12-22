b8368bc9bfc083e245c3812687dfc212e17e5cc8
minjo101 <joosungmin@gmail.com>
2019-12-21 22:42:11 -0800

    Ch2_5_fine_tune_your_model.py

--- Python/Hands on Machine Learning/Ch2_2_explore_data.py
+++ Python/Hands on Machine Learning/Ch2_2_explore_data.py
@@ -52,7 +52,7 @@ imputer = SimpleImputer(strategy="median")
 housing_num = housing.drop("ocean_proximity", axis = 1) # since median can be computed on numerical variables
 imputer.fit(housing_num)
 # the imputer stores the median values in its statistics_ instance variable
-print(imputer.statistics_)
+# print(imputer.statistics_)
 # now we can use the 'trained' imputer to transform the training set by replacing missing values with the learned medians:
 X = imputer.transform(housing_num)
 
@@ -68,4 +68,28 @@ from sklearn.preprocessing import OneHotEncoder
 cat_encoder = OneHotEncoder()
 housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
 housing_cat_1hot.toarray()
-print(cat_encoder.categories_)
+# print(cat_encoder.categories_)
+
+
+## Custom Transformers
+# **Not really sure about this one. Come back to this
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self # nothing else to do
+    def transform(self, X, y=None):
+        rooms_per_household = X[:,rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
